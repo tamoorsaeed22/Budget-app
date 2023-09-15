6b0d2b405a11cb455b3d102aedd2a025295f2b9b
Tamoor saeed <115831124+tamoorsaeed22@users.noreply.github.com>
2023-09-15 19:01:25 +0500

    Complete the project

---
+++ app/controllers/fonts_controller.rb
@@ -0,0 +1,6 @@
+class FontsController < ApplicationController
+  def show
+    # Set the name variable to the value of the Proxima font file.
+    'Proxima'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
+    get 'home/index'
+    get '/fonts/:name.otf', to: 'fonts#show'
+    resources :payments, only: [:new, :index, :create]
+    resources :categories, except: [:update, :edit]
+    devise_for :users
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+    # Defines the root path route ("/")
+    devise_scope :user do
+      authenticated :user do
+        root "categories#index", as: :authenticated_root
+      end
+    
+      unauthenticated do
+        root 'home#index', as: :unauthenticated_root
+      end
+    end
+  end
