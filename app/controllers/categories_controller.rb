eec2f8f0104ea09f5aa496ed7d07c4b5784dbfdc
Tamoor saeed <115831124+tamoorsaeed22@users.noreply.github.com>
2023-09-16 02:41:02 +0500

    modify controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,7 +9,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1 or /categories/1.json
   def show; end
 
-  # GET /catgories/new
+  # GET /categories/new
   def new
     @category = Category.new
   end
--- app/controllers/fonts_controller.rb
+++ app/controllers/fonts_controller.rb
@@ -1,6 +1,6 @@
 class FontsController < ApplicationController
   def show
-    # Set th name variable to the value of the Proxima font file.
+    # Set the name variable to the value of the Proxima font file.
     'Proxima'
   end
 end
